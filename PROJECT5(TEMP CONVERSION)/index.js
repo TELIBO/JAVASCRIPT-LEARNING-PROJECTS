fb4f691c8cc4fae8046b4391011f539438d2a53d
TELIBO <138214709+TELIBO@users.noreply.github.com>
2024-06-18 15:17:29 +0530

    cc

--- PROJECT5(TEMP CONVERSION)/index.js
+++ PROJECT5(TEMP CONVERSION)/index.js
@@ -5,10 +5,12 @@ document.getElementById("b1").onclick = function() {
         val = parseFloat(document.getElementById("input").value);
         if (val >= 40) {
             emotion = "it's getting hot";
+            document.getElementById("emotion").innerHTML = `<img style=" width:400px;height:300px;" src="images/gettyimages-1241852095-612x612.jpg">`;
         } else if (val > 20 && val < 40) {
             emotion = "it's normal";
         } else {
             emotion = "it's getting cold";
+            document.getElementById("emotion").innerHTML = `<img style=" width:400px;height:300px;" src="images/2608971.jpg">`;
         }
         val = ((val * 9) / 5) + 32;
         document.getElementById("p1").textContent = `${val.toFixed(2)} Fahreneit, ${emotion}`;
