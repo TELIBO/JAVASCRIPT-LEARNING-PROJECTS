4db018788e18119c08e62337548319457baefddb
TELIBO <138214709+TELIBO@users.noreply.github.com>
2024-06-26 19:34:59 +0530

    l

--- PROJECT14(IMAGE SLIDER)/index.js
+++ PROJECT14(IMAGE SLIDER)/index.js
@@ -1,22 +1,23 @@
 let slidenumber=0;
-
+const slide = document.querySelector(`.images`);
+const slideWidth = 800; 
 document.querySelector(".next").addEventListener("click",function(){
     slidenumber++;
     if(slidenumber>2){
         slidenumber=0;
+        slide.style.transition = "transform 0s";
     }
-    const slide = document.querySelector(`.images`);
-    const slideWidth = 800; 
-
+    else{
+    slide.style.transition = "transform 1s";}
     slide.style.transform = `translateX(-${slidenumber * slideWidth}px)`; 
 })
 document.querySelector(".prev").addEventListener("click",function(){
     slidenumber--;
     if(slidenumber<0){
         slidenumber=2;
+        slide.style.transition = "transform 0s";
     }
-    const slide = document.querySelector(`.images`);
-    const slideWidth = 800; 
-
+    else{
+        slide.style.transition = "transform 1s";}
     slide.style.transform = `translateX(-${slidenumber * slideWidth}px)`; 
 })
